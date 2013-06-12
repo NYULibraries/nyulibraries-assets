cf5c596c13ad7b99e0092b71f40ecea28279e65f
Scot Dalton <scotdalton@gmail.com>
2013-06-11 22:19:09 -0400

    Update templates, add some tests. :dog:

--- test/functional/nyulibraries_controller_test.rb
+++ test/functional/nyulibraries_controller_test.rb
@@ -4,20 +4,38 @@ class NyulibrariesControllerTest < ActionController::TestCase
   test "should get application" do
     get :application
     assert_response :success
+    assert_select "title", "Dummy Application"
+    assert_select "header > div.suite > span", 0
+    assert_select "header > div.application > span", {count: 1, text: "Dummy Application"}
+    assert_select "header", 1
   end
 
   test "should get bobcat" do
     get :bobcat
     assert_response :success
+    assert_select "title", "BobCat"
+    assert_select "header", 1
+    assert_select "header > div.suite > span", {count: 1, text: "BobCat"}
+    assert_select "header > div.application > span", {count: 1, text: "Dummy Application"}
+    assert_select "footer", 0
   end
 
   test "should get eshelf" do
     get :eshelf
     assert_response :success
+    assert_select "title", "BobCat"
+    assert_select "header", 1
+    assert_select "header > div.suite > span", {count: 1, text: "BobCat"}
+    assert_select "header > div.application > span", {count: 1, text: "Dummy Application"}
+    assert_select "footer", 0
   end
 
   test "should get print" do
     get :print
     assert_response :success
+    assert_select "title", "Dummy Application"
+    assert_select "h1", {count: 1, text: "Dummy Application"}
+    assert_select "header", 0
+    assert_select "footer", 0
   end
 end
