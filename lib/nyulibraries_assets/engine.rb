d6a8b68b0da6c15be2d2eb287d57828a47dcd0b0
Scot Dalton <scotdalton@gmail.com>
2013-06-17 12:27:18 -0400

    Engines use the app, not just config, so need to make sure we're referring to that correctly.  I'm a moron. :sparkling_heart:

--- lib/nyulibraries_assets/engine.rb
+++ lib/nyulibraries_assets/engine.rb
@@ -10,7 +10,7 @@ module NYULibrariesAssets
         # Precompile institutional stylesheets
         Institutions.institutions.each_value do |institution|
           stylesheet = institution.views["css"]
-          app.config.assets.precompile << stylesheet unless (stylesheet.blank? or config.assets.precompile.include?(stylesheet))
+          app.config.assets.precompile << stylesheet unless (stylesheet.blank? or app.config.assets.precompile.include?(stylesheet))
         end
         app.config.compass.sprite_load_path << File.join(self.root, "lib", "assets", "images")
         path = self.root
--- lib/nyulibraries_assets/version.rb
+++ lib/nyulibraries_assets/version.rb
@@ -1,3 +1,3 @@
 module NYULibrariesAssets
-  VERSION = "1.1.2"
+  VERSION = "1.1.3"
 end
