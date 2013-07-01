f22c70cb09e6be076c95921a6a6193f82189e03b
Scot Dalton <scotdalton@gmail.com>
2013-07-01 10:05:26 -0400

    Add '.css' suffix so we actually get them :cake: Fixes #25 :rainbow:

--- lib/nyulibraries_assets/engine.rb
+++ lib/nyulibraries_assets/engine.rb
@@ -9,7 +9,7 @@ module NYULibrariesAssets
         app.config.assets.precompile += ['print.css']
         # Precompile institutional stylesheets
         Institutions.institutions.each_value do |institution|
-          stylesheet = institution.views["css"]
+          stylesheet = "#{institution.views["css"]}.css"
           app.config.assets.precompile << stylesheet unless (stylesheet.blank? or app.config.assets.precompile.include?(stylesheet))
         end
         app.config.compass.sprite_load_path << File.join(self.root, "lib", "assets", "images")
