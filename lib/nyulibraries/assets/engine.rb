e5001fa82854c87336997fba42d6168d1ceaaa91
hab278 <hab278@nyu.edu>
2014-04-11 16:04:56 -0400

    Redefining compass-rails method that causes catastrophic failure

--- lib/nyulibraries/assets/engine.rb
+++ lib/nyulibraries/assets/engine.rb
@@ -3,8 +3,8 @@ module Nyulibraries
     class Engine < ::Rails::Engine
       require 'compass-rails'
       require 'bootstrap-sass'
-      require 'mustache/railtie'
       require 'institutions'
+      require 'sprockets/railtie'
       initializer "#{engine_name}.asset_pipeline" do |app|
         app.config.assets.precompile += ['print.css']
         # Precompile institutional stylesheets
@@ -17,6 +17,22 @@ module Nyulibraries
         ActiveSupport.on_load(:action_controller) do
           append_view_path File.join(path, "app", "templates")
         end
+
+
+        module Sass::Script::FunctionsPatch
+          def generated_image_url(path, only_path = nil)
+            cachebust_generated_images
+            path
+          end
+
+          def cachebust_generated_images
+            generated_images_path = Rails.root.join(Compass.configuration.generated_images_dir).to_s
+          end
+        end
+        class Sass::Script::Functions::EvaluationContext
+          include Sass::Script::FunctionsPatch
+        end
+
       end
     end
   end
