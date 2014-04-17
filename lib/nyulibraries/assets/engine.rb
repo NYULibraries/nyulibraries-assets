cdff55fa36b68cb708a645587b1900986b2e3bc5
hab278 <hab278@nyu.edu>
2014-04-17 14:51:08 -0400

    Monkey patch bootstrap-sass and rails-sass to work with our little concoction

--- lib/nyulibraries/assets/engine.rb
+++ lib/nyulibraries/assets/engine.rb
@@ -1,10 +1,10 @@
 module Nyulibraries
   module Assets
     class Engine < ::Rails::Engine
-      require 'compass-rails'
       require 'bootstrap-sass'
       require 'institutions'
       require 'sprockets/railtie'
+      require 'compass-rails'
       initializer "#{engine_name}.asset_pipeline" do |app|
         app.config.assets.precompile += ['print.css']
         # Precompile institutional stylesheets
@@ -16,6 +16,16 @@ module Nyulibraries
         path = self.root
         ActiveSupport.on_load(:action_controller) do
           append_view_path File.join(path, "app", "templates")
+        end
+        module Sass::Script::Functions
+          include ActionView::Helpers::AssetUrlHelper
+          def generated_image_url(path, only_path = nil)
+            cachebust_generated_images
+            Sass::Script::String.new(asset_url(path),:string)
+          end
+          def image_path(source, options = {})
+            ::Sass::Script::String.new asset_url(source).to_s, :string
+          end
         end
       end
     end
