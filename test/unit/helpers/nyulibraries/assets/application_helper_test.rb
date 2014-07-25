32044d4eb1b92dc3e878418f70a319d9294b2d46
Scot Dalton <scotdalton@gmail.com>
2014-07-25 13:13:21 -0400

    Comment on why we're including omissions and fix a typo in the skips

--- test/unit/helpers/nyulibraries/assets/application_helper_test.rb
+++ test/unit/helpers/nyulibraries/assets/application_helper_test.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+# For skipping tests
 require 'test/unit/omission'
 module Nyulibraries
   module Assets
@@ -8,14 +9,14 @@ module Nyulibraries
       end
 
       test "application stylesheet" do
-        skip("Skipping for rails 4, Rails 4 handles html tags differntly") if(ActiveRecord::VERSION::MAJOR > 3)
+        skip("Skipping for rails 4, Rails 4 handles html tags differently") if(ActiveRecord::VERSION::MAJOR > 3)
         assert_equal("<link href=\"/stylesheets/application.css\" " +
           "media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />",
             application_stylesheet)
       end
 
       test "application javascript" do
-        skip("Skipping for rails 4, Rails 4 handles html tags differntly") if(ActiveRecord::VERSION::MAJOR > 3)
+        skip("Skipping for rails 4, Rails 4 handles html tags differently") if(ActiveRecord::VERSION::MAJOR > 3)
         assert_equal("<script src=\"/javascripts/application.js\" " +
           "type=\"text/javascript\"></script>", application_javascript)
       end
