090998fb7ef99be346976df500f299e6dd206a1f
B Alter <barnaby.alter@gmail.com>
2016-03-10 14:48:05 -0500

    Remove secret_token and use secrets to shut up deprecation warning

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'eb637c7ec2f2f3b24b515c365bcf73f40932f17859e6b202ba5d6b51e90b9c256c6ba6d34a4ee4a63ade7db7435df416d1ad0c0bc1b8f6767bed27b4261d1f3a'
